201668f0297edf400bb7337c8b1ea2aef88767e2
Yashraj Singh Solanki <yes20sh@gmail.com>
2025-06-04 17:10:06 +0000

    feat: backend app.js

--- server/src/app.js
+++ server/src/app.js
@@ -16,10 +16,11 @@ app.use(express.json()); // 🔹 Needed to parse JSON request bodies
 app.use(cookieParser());
 
 // 🔐 CORS Configuration (recommended)
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-  credentials: true,
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+//   credentials: true,
+// }));
 
 // Routes
 app.use('/api/auth', authRoutes);
