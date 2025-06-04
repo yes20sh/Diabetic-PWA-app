d835468db1bb5405ba7e02f8a8f51da965422690
Yashraj Singh Solanki <yes20sh@gmail.com>
2025-06-04 20:06:18 +0000

    feat: update vte config and package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-   server: {
-       host: 'localhost',
-    port: 3000, 
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  server: {
+    // For local dev: default is localhost (127.0.0.1)
+    host: 'localhost',  
+    port: 3000
+  },
+  build: {
+    // Explicit output folder for production build
+    outDir: 'dist'
   }
 })
