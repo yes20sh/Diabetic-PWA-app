a8bbd3b618d45b2acd61a6ce051e344db7fcb546
Yashraj Singh Solanki <yes20sh@gmail.com>
2025-06-04 20:10:48 +0000

    update vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,13 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  server: {
-    // For local dev: default is localhost (127.0.0.1)
-    host: 'localhost',  
-    port: 3000
-  },
-  build: {
-    // Explicit output folder for production build
-    outDir: 'dist'
-  }
 })
