683719d66c16773f6e9720ecc9e6eaa71a3ed2a2
Yashraj Singh Solanki <yes20sh@gmail.com>
2025-06-04 19:10:57 +0000

    feat: update

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -86,7 +86,7 @@ export const login = async (req, res) => {
 res.cookie("token", token, {
   httpOnly: true,
   secure: true,
-  sameSite: 'Lax',
+  sameSite: 'None',
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 });
 
