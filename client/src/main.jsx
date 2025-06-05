53b548b6f07c5d9f1dfa160bd4508fe3bd351679
Yashraj Singh Solanki <yes20sh@gmail.com>
2025-06-05 10:57:37 +0000

    feat: update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,8 +6,8 @@ import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
+
     <App />
-    </AuthProvider>
+
   </StrictMode>,
 )
